db75fbe0a82bdc12d68f59943e39b619a47a0942
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-22 13:17:08 -0400

    LAB

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
 // Your code here.
+const getFirstName = function(obj) {
+  return obj.firstName
+}
 
+const getLastName = function(obj) {
+  return obj.lastName
+}
 
+const getFullName = function(obj) {
+  return `${obj.firstName} ${obj.lastName}`
+}
 
+const setFirstName = function(obj, name) {
+  obj.firstName = name
+}
+
+const setAge = function(obj, age) {
+  obj.age = age
+}
+
+const giveBirthday = function(obj) {
+  if (obj.age === undefined) {
+    obj.age = 1
+  } else {
+    obj.age ++
+  }
+  return obj
+}
+
+const marry = function(person1, person2) {
+  person1.married = true
+  person1.spouseName = `${person2.firstName} ${person2.lastName}`
+  person2.married = true
+  person2.spouseName = `${person1.firstName} ${person1.lastName}`
+}
+
+const divorce = function(person1, person2) {
+  person1.married = false
+  delete person1.spouseName
+  person2.married = false
+  delete person2.spouseName
+}
 
 
 // Our code here. Don't touch!
